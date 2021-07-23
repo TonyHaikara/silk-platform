41e9b1ae593d5cae9ed2ab4425ee001af2a8e3a4
Yannick Warnier <ywarnier@beeznest.org>
2021-07-23 10:39:54 +0200

    Tests: Add basic behat tests

---
+++ tests/behat/README.md
@@ -0,0 +1,54 @@
+In order to run behat tests locally with the right support for browser
+and JS environments under Linux, you will need to:
+
+- Have Java installed (see notes below)
+- Download Selenium Standalone Server v3.*
+
+http://www.seleniumhq.org/download/
+
+And run it with the following command:
+
+```
+java -jar /my-dir/selenium-server-standalone-3.1.0.jar
+```
+
+- Download the Chrome driver, unzip and copy into /usr/bin
+
+Check the latest version at https://sites.google.com/a/chromium.org/chromedriver/downloads,
+then adapt the following command to the latest version. Use a version that matches your version of the Chrome browser.
+
+```
+cd /tmp && wget https://chromedriver.storage.googleapis.com/2.35/chromedriver_linux64.zip && unzip chromedriver_linux64.zip && sudo mv chromedriver /usr/local/bin
+```
+
+### SILKC configuration
+
+- An administrator user should be created with these parameters:
+    - Username "admin"
+    - Password "admin"
+    - First name "John"
+    - Last name "Doe"
+    - user_id = 3 (this one is set when you install SILKC with its fixtures, but just in case...)
+
+- Edit the tests/behat/behat.yml file and update the base_url param with your own SILKC local URL.
+
+### Run tests
+
+To run all features:
+
+```
+# /var/www/html/silkc
+cd tests/behat
+ ../../vendor/behat/behat/bin/behat -v
+ ```
+
+To run an specific feature:
+
+```
+../../vendor/behat/behat/bin/behat features/user.feature
+```
+
+## Java versions
+
+Not all java versions will work. For Ubuntu, `sudo apt install openjdk-11-jdk openjdk-11-jre` should do, but OpenJDK 17 will not work, for example.
+If you have several versions installed, you can update the "active" version with `sudo update-java-alternatives -l` to see the possibilities, then `sudo update-java-alternatives -s java-1.11.0-openjdk-amd64` or something like that to set it. Beware this can have a big impact on other things you run with Java (like your IDE?) so maybe think about undoing this later on...
---
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -0,0 +1,256 @@
+<?php
+
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Features context. (MinkContext extends BehatContext)
+ */
+class FeatureContext extends MinkContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets its own context object.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Given /^I am a platform administrator$/
+     */
+    public function iAmAPlatformAdministrator()
+    {
+        $this->iAmLoggedAs('admin', 'admin');
+    }
+
+    /**
+     * @Given /^I am an institution$/
+     */
+    public function iAmAnInstitution()
+    {
+        $this->iAmLoggedAs('institution', 'institution');
+    }
+
+    /**
+     * @Given /^I am a final user$/
+     */
+    public function iAmAFinalUser()
+    {
+        $this->iAmLoggedAs('user', 'user');
+    }
+
+    /**
+     * @Given /^I am logged as "([^"]*):([^"]*)"$/
+     */
+    public function iAmLoggedAs($username, $password)
+    {
+        //$this->visit('/logout');
+        $this->visit('/login');
+        $this->fillField('login', $username);
+        $this->fillField('password', $password);
+        $this->pressButton('Sign in');
+        $this->waitForThePageToBeLoaded();
+    }
+
+    /**
+     * Checks, that element with specified CSS doesn't exist on page
+     *
+     * @Then /^(?:|I )should not see an error$/
+     */
+    public function iShouldNotSeeAnError()
+    {
+        $this->assertSession()->pageTextNotContains('Internal server error');
+        $this->assertSession()->pageTextNotContains('error');
+        $this->assertSession()->pageTextNotContains('Oops');
+        //$this->assertSession()->elementNotExists('css', '.alert-danger');
+    }
+
+    /**
+     * @Given /^I am not logged in$/
+     */
+    public function iAmNotLoggedIn()
+    {
+        $this->visit('/logout');
+    }
+
+    /**
+     * @Then /^I fill in editor field "([^"]*)" with "([^"]*)"$/
+     */
+    public function iFillInWysiwygOnFieldWith($locator, $value)
+    {
+        // Just in case wait that ckeditor is loaded
+        $this->getSession()->wait(2000);
+
+        $el = $this->getSession()->getPage()->findField($locator);
+        $fieldId = $el->getAttribute('id');
+
+        if (empty($fieldId)) {
+            throw new Exception(
+                'Could not find an id for field with locator: '.$locator
+            );
+        }
+
+        $this->getSession()->executeScript(
+            "setContentFromEditor(\"$fieldId\", \"$value\");"
+        );
+    }
+
+    /**
+     * @Given /^I fill hidden field "([^"]*)" with "([^"]*)"$/
+     */
+    public function iFillHiddenFieldWith($field, $value)
+    {
+        $this->getSession()->getPage()->find(
+            'css',
+            'input[name="'.$field.'"]'
+        )->setValue($value);
+    }
+
+    /**
+     * @When /^(?:|I )confirm the popup$/
+     */
+    public function confirmPopup()
+    {
+        // See
+        // https://gist.github.com/blazarecki/2888851
+        /** @var \Behat\Mink\Driver\Selenium2Driver $driver Needed because no cross-driver way yet */
+        $this->getSession()->getDriver()->getWebDriverSession()->accept_alert();
+    }
+
+    /**
+     * @When /^(?:|I )wait for the page to be loaded$/
+     */
+    public function waitForThePageToBeLoaded()
+    {
+        $this->getSession()->wait(4000);
+    }
+
+    /**
+     * @When /^wait very long for the page to be loaded$/
+     */
+    public function waitVeryLongForThePageToBeLoaded()
+    {
+        $this->getSession()->wait(8000);
+    }
+
+    /**
+     * @When /^wait the page to be loaded when ready$/
+     */
+    public function waitVeryLongForThePageToBeLoadedWhenReady()
+    {
+        $this->getSession()->wait(9000, "document.readyState === 'complete'");
+    }
+
+    /**
+     * @When /^I check the "([^"]*)" radio button$/
+     */
+    public function iCheckTheRadioButton($radioLabel)
+    {
+        $radioButton = $this->getSession()->getPage()->findField($radioLabel);
+        if (null === $radioButton) {
+            throw new Exception("Cannot find radio button ".$radioLabel);
+        }
+        $this->getSession()->getDriver()->click($radioButton->getXPath());
+    }
+
+    /**
+     * @When /^I check radio button with label "([^"]*)"$/
+     */
+    public function iCheckTheRadioButtonWithLabel($label)
+    {
+        $this->getSession()->executeScript("
+            $(function() {
+                $(':contains(\$label\")').parent().find('input').prop('checked', true);
+            });
+        ");
+    }
+
+     /**
+     * Clicks link with specified id|title|alt|text
+     * Example: When I follow "Log In"
+     * Example: And I follow "Log In"
+     *
+     * @When /^(?:|I )focus "(?P<link>(?:[^"]|\\")*)"$/
+     */
+    public function focus($input)
+    {
+        $input = $this->getSession()->getPage()->findField($input);
+        $input->focus();
+    }
+
+    /**
+     * @Given /^I check the "([^"]*)" radio button with "([^"]*)" value$/
+     */
+    public function iCheckTheRadioButtonWithValue($element, $value)
+    {
+        $this->getSession()->executeScript("
+            $(function() {
+                $('input[type=\"radio\"][name=".$element."][value=".$value."]').prop('checked', true);
+            });
+        ");
+
+        return true;
+    }
+
+    /**
+     * @Given /^I check the "([^"]*)" radio button selector$/
+     */
+    public function iCheckTheRadioButtonBasedInSelector($element)
+    {
+        $this->getSession()->executeScript("
+            $(function() {
+                $('$element').prop('checked', true);
+            });
+        ");
+
+        return true;
+    }
+
+    /**
+     * @Then /^I should see an icon with title "([^"]*)"$/
+     */
+    public function iShouldSeeAnIconWithTitle($value)
+    {
+        $el = $this->getSession()->getPage()->find('xpath', "//img[@title='$value']");
+        if (null === $el) {
+            throw new Exception(
+                'Could not find an icon with title: '.$value
+            );
+        }
+        return true;
+    }
+    /**
+     * @Then /^I should not see an icon with title "([^"]*)"$/
+     */
+    public function iShouldNotSeeAnIconWithTitle($value)
+    {
+        $el = $this->getSession()->getPage()->find('xpath', "//img[@title='$value']");
+        if (null === $el) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @Then /^I save current URL with name "([^"]*)"$/
+     */
+    public function saveUrlWithName($name)
+    {
+        $url = $this->getSession()->getCurrentUrl();
+        $this->getSession()->setCookie($name, $url);
+    }
+
+    /**
+     * @Then /^I visit URL saved with name "([^"]*)"$/
+     */
+    public function visitSavedUrlWithName($name)
+    {
+        $url = $this->getSession()->getCookie($name);
+        echo $url;
+        if (empty($url)) {
+            throw new Exception("Url with name: $name not found");
+        }
+        $this->visit($url);
+    }
+}
